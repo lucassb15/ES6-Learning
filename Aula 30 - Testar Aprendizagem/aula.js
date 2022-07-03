3810915abac1cc87f15eea27a0965b5cf8f4462c
Lucas Soares <soares_lucas13@hotmail.com>
2022-07-03 14:37:55 -0300

    ES6

--- Aula 30 - Testar Aprendizagem/aula.js
+++ Aula 30 - Testar Aprendizagem/aula.js
@@ -2,3 +2,23 @@
 console.log("EcmaScript 6");
 console.log(" ");
 
+// criar carro
+function criarCarro(marca, nome, cor, portas){
+    console.log(`O carro ${marca} ${nome}, de cor ${cor} tem ${portas} portas!`);
+}
+
+const carro = ['Honda', 'Civic 2022', 'Preta' , 4];
+
+criarCarro(...carro);
+
+// Bomba
+function Som (kind, sound) {
+    this.kind = kind;
+    this.sound = sound;
+}
+
+Som.prototype.hello = function() {
+    console.log(`${this.sound} is a ${this.kind}`);
+}
+
+const bomb = new Som('Dinamite', 'Booooomm!');
